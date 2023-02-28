f92e39945f14d19c3365d387b5e49f1365d42f61
Anna Kirillova <a.kirillova1812@gmail.com>
2023-02-28 18:51:16 +0400

    Part 1 solution was added

---
+++ src/Day6/Part1.java
@@ -0,0 +1,61 @@
+package Day6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+public class Part1 {
+
+    final static int ROWS = 1000;
+    final static int COLUMNS = 1000;
+    static int[][] grid = new int [ROWS][COLUMNS];
+
+    public static void main(String[] args) throws IOException {
+        String path = ".\\src\\Day6\\data.txt";
+        final String[] instructions = Files.readString(Path.of(path)).lines().toArray(String[]::new);
+
+        for (String str : instructions) {
+            final String regex = "^(toggle|turn off|turn on) (\\d+,\\d+) (through) (\\d+,\\d+)$";
+
+            String todo = str.replaceAll(regex, "$1");
+            String[] coord1 = str.replaceAll(regex, "$2").split(",");
+            String[] coord2 = str.replaceAll(regex, "$4").split(",");
+
+            setUpLights(todo, coord1, coord2);
+        }
+
+        long result= Arrays.stream(grid).flatMapToInt(Arrays::stream).filter(a -> a == 1).count();
+
+        System.out.println(result);
+
+    }
+
+    public static void setUpLights(String todo, String [] coord1, String [] coord2) {
+
+        int beginX = Integer.parseInt(coord1[0]);
+        int beginY = Integer.parseInt(coord1[1]);
+        int endX = Integer.parseInt(coord2[0]);
+        int endY = Integer.parseInt(coord2[1]);
+
+        for (int i = beginX; i <= endX; i++) {
+            for (int j = beginY; j <= endY; j++) {
+                switch (todo) {
+                    case "turn off":
+                        grid[i][j] = 0;
+                        break;
+                    case "turn on":
+                        grid[i][j] = 1;
+                        break;
+                    case "toggle":
+                        if (grid[i][j] == 1) {
+                            grid[i][j] = 0;
+                        } else {
+                            grid[i][j] = 1;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
